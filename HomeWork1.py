2623b21a6f128e5c6ff003853ceaa662ff0fca45
Oleksa Korol <oleksandrwinner@gmail.com>
2019-10-03 15:32:59 +0300

    Home work task 1

---
+++ HomeWork1.py
@@ -0,0 +1,9 @@
+number = input ("Введіть Ваш вік >>> ")
+number = int (number)
+if number < 16 :
+    print ("Привіт;")
+elif number < 30 :
+    print ("Вітання;")
+else:
+    number > 30
+    print ("Добрий день.")
